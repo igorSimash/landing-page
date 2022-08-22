6b1430b965617b45a8010b52b76906a30c139c8f
igorSimash <vigorochok@gmail.com>
2022-08-22 18:51:57 +0300

    some little changes

--- src/components/section/section-1/Section1.jsx
+++ src/components/section/section-1/Section1.jsx
@@ -6,7 +6,7 @@ const Section1 = () => {
     return (
         <section className={'section-1'}>
             <div className={'content'}>
-                <Heading>Test assignment for front-end developer</Heading>
+                <Heading>Front-end developer</Heading>
                 <p>What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.</p>
                 <Button onClick={() => document.querySelector('#postH1').scrollIntoView()}>
                     Sign up
